ed5f71a2c2eea1a160142d9d15fa78b8467ab5bb
angular6471 <angular6471@caelumBSB03-18>
2016-07-18 12:59:59 -0300

    commit fim da aula

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,41 @@
+/*
+angular
+    .module('alurapic')
+    .controller('FotosController', function($scope) {
+        $scope.fotos = [
+            {titulo: 'Leão',
+            url : 'http://fundosanimais.com/Minis/leoes.jpg'},
+            {titulo: 'outro',
+             url: 'http://fundosanimais.com/Minis/leoes.jpg'}
+        
+        ];
+    });
+*/
+/*
+angular
+    .module('alurapic')
+    .controller('FotosController', function($scope, $http){
+        $scope.fotos = [];
+        var promise = $http.get('/v1/fotos');
+    
+        promise.then(function(retorno){
+            $scope.fotos = retorno.data;
+        });
+    
+        promise.catch(function(erro){
+            console.error(erro);
+        });
+    });
+*/
+angular
+    .module('alurapic')
+    .controller('FotosController', function($scope, $http){
+        $scope.fotos = [];
+        $http.get('/v1/fotos').success(
+            function(data){
+                $scope.fotos = data;
+            }).error(
+            function(erro){
+                console.log(erro);
+            });    
+    });
